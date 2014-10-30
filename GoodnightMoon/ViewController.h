67762ed3588d9ff2d415f1b1314e9016131fa5ff
Alexey Emelyanov <emelyanoff@gmail.com>
2014-10-30 10:45:49 -0700

    Initial Commit

---
+++ GoodnightMoon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoodnightMoon
+//
+//  Created by Alex on 10/30/14.
+//  Copyright (c) 2014 Alexey Emelyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoodnightMoon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoodnightMoon
+//
+//  Created by Alex on 10/30/14.
+//  Copyright (c) 2014 Alexey Emelyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
